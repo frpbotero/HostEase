65b794e266aaac86f540efefec36dabd2fe330a4
edgarrps <edgarrps@gmail.com>
2023-05-09 22:57:45 -0400

    feat: Created a new page and route with redirection to User Dashboard, aka /dashboard :: Dashboard.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import App from './App'
 import Login from './pages/Login'
 import Calendar from './pages/Calendar'
+import Dashboard from './pages/Dashboard'
 
 export default function () {
     return (
@@ -9,6 +10,7 @@ export default function () {
             <Route path='/' element={<App />} />
             <Route path='/login' element={<Login />} />
             <Route path='/calendar' element={<Calendar />} />
+            <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
     )
 }
